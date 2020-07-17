3788db455c65c8ec83a1c4b9ff87593cd605880b
Thomas Maschler <tmaschler@wri.org>
2020-07-16 21:40:38 -0400

    update version number in setup.py
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tileputty",
-    version="0.2.0",
+    version="0.2.2",
     description="Tool to upload a tile cache to AWS S3",
     url="https://github.com/wri/tileputty",
     packages=["tileputty"],
