9a0a16e06f0019273262b90765ea9bb8805403c3
Thomas Maschler <tmaschler@wri.org>
2021-01-15 13:01:47 -0500

    allow user to specify number of cores. Switch to typer
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- tileputty/upload_tiles.py
+++ tileputty/upload_tiles.py
@@ -6,12 +6,12 @@ from logging import Logger
 from typing import Iterator
 
 import boto3
-import click
 from botocore.config import Config
-from parallelpipe import stage
+from parallelpipe import Stage
+from typer import Argument, Option, run
 
 LOGGER: Logger = logging.getLogger("tileputty")
-CORES: int = multiprocessing.cpu_count()
+CORES: int = int(os.environ.get("CORES", multiprocessing.cpu_count()))
 
 # File headers for different formats
 MVT_META = {
@@ -32,16 +32,15 @@ ARGS = {
 }
 
 
-@click.command()
-@click.option("--bucket", default="gfw-tiles", help="AWS Bucket")
-@click.option("--dataset", help="Dataset Name")
-@click.option("--version", help="Dataset Version ID")
-@click.option("--implementation", default="default", help="Tile Cache Implementation")
-@click.argument("tile_cache")
-def cli(
-    tile_cache: str, dataset: str, version: str, bucket: str, implementation: str
+def main(
+    tile_cache: str = Argument(..., help="Path to local tile cache"),
+    dataset: str = Option(..., help="Dataset Name"),
+    version: str = Option(..., help="Dataset Version ID"),
+    bucket: str = Option("gfw-tiles", help="AWS Bucket"),
+    implementation: str = Option(..., help="Tile Cache Implementation"),
+    cores: int = Option(..., help="Number or processes to use to upload tiles"),
 ) -> None:
-    """ TILE_CACHE: path to local tile cache """
+    """Upload a local tile cache to S3."""
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(
@@ -57,6 +56,7 @@ def cli(
         dataset=dataset,
         version=version,
         implementation=implementation,
+        cores=cores,
     )
 
 
@@ -66,41 +66,32 @@ def upload_tiles(
     version: str,
     bucket: str = "gfw-tiles",
     implementation: str = "default",
+    cores: int = CORES,
 ) -> None:
-    """
-    :param tile_cache: Path to local tile cache
-    :param layer: Layer name
-    :param version: Layer version number
-    :param bucket: AWS S3 bucket name (default: gfw-tiles)
-    :param option: Layer option (default: default)
-    :param ext: Tile cache format (default: mvt)
-    :param update_latest: Update latest file in tile cache root (default: False)
-    :return: None
-    """
+    """Upload a local tile cache to S3."""
 
     LOGGER.info(
-        f"Upload tile cache to {dataset}/{version}/{implementation} using {CORES} processes"
+        f"Upload tile cache to {dataset}/{version}/{implementation} using {cores} processes"
     )
 
     # pipe files
-    pipe = _files(tile_cache) | _stage_copy(
-        tile_cache, bucket, dataset, version, implementation
-    )
+    pipe = get_tiles(tile_cache) | Stage(
+        copy_tiles, tile_cache, bucket, dataset, version, implementation
+    ).setup(workers=cores)
 
     # collect results
     for output in pipe.results():
         LOGGER.debug(output)
 
 
-def _files(tile_cache: str) -> Iterator[str]:
+def get_tiles(tile_cache: str) -> Iterator[str]:
     """Collect all files in tile cache."""
     for root, dirs, files in os.walk(tile_cache):
         for f in files:
             yield os.path.join(root, f).replace("\\", "/")
 
 
-@stage(workers=CORES)
-def _stage_copy(
+def copy_tiles(
     files, tile_cache, bucket, dataset, version, implementation
 ) -> Iterator[str]:
     """Upload files to S3 and update file header."""
@@ -125,5 +116,5 @@ def _stage_copy(
         yield s3_path
 
 
-if __name__ == "__main__":
-    cli()
+def cli():
+    run(main)
